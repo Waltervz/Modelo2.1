8e3359428d2dcf7c6145491d0a90ea0bf248d36a
Walter <waltervz278@gmail.com>
2023-07-20 14:45:35 -0300

    subiendo version final

---
+++ main.js
@@ -0,0 +1,28 @@
+const app = document.getElementById('typewriter');
+
+const typewriter = new Typewriter(app, {
+    loop: true,
+    delay:75,
+});
+
+
+typewriter
+.typeString('Oppenheimer y Asociados')
+.pauseFor(200)
+.start();
+
+function temaOscuro() {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
+
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class" , "bi bi-moon-fill");
+}
+
+
+
+const cambiarTema = () => {
+    document.querySelector("body").getAttribute("data-bs-theme") === "light"? temaOscuro() : temaClaro();
+}
